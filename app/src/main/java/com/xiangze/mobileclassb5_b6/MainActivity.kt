0550c58e96bd450086d54367f2acfabdbb8a9f75
nathalie7980 <londonhippos@gmail.com>
2022-12-15 12:02:56 +0800

    ViewPager with TabLayout

--- app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
+++ app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
@@ -2,12 +2,35 @@ package com.xiangze.mobileclassb5_b6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.xiangze.mobileclassb5_b6.adapters.PageAdapter
+import com.xiangze.mobileclassb5_b6.databinding.ActivityMainBinding
+import com.xiangze.mobileclassb5_b6.fragments.FirstFragment
+import com.xiangze.mobileclassb5_b6.fragments.SecondFragment
 
 
 //nathalie
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = PageAdapter(
+            listOf(FirstFragment(), SecondFragment()),
+            supportFragmentManager,
+            this.lifecycle
+        )
+
+        binding.vpPages.adapter = adapter
+
+        TabLayoutMediator(binding.tlHome, binding.vpPages) { tab, position ->
+            when(position) {
+                0 -> tab.text = "First Fragment"
+                else -> tab.text = "Second Fragment"
+            }
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/xiangze/mobileclassb5_b6/fragments/FirstFragment.kt
@@ -0,0 +1,21 @@
+package com.xiangze.mobileclassb5_b6.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.xiangze.mobileclassb5_b6.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+}
