af06228977b3cfe373aeaa4df4652be1bbcf9c85
Lo0425 <chingclo.lcc@gmail.com>
2022-12-15 11:17:23 +0800

    testing

--- app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
+++ app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
@@ -3,6 +3,7 @@ package com.xiangze.mobileclassb5_b6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Chia Ching
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
