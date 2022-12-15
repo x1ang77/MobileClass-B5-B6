175a699b6b939755cb4716237fc8c21d082178ea
Joel Seah <quanshenjoelmichael21s@forwardschool.edu.my>
2022-12-15 11:16:45 +0800

    Testing

--- app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
+++ app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
@@ -3,6 +3,8 @@ package com.xiangze.mobileclassb5_b6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Joel
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
