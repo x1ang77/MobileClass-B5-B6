87d18fa55761ab758ec3838ef6082008f0b6f6ed
Lo0425 <chingclo.lcc@gmail.com>
2022-12-15 12:02:59 +0800

    Added Tablayout

---
+++ app/src/main/java/com/xiangze/mobileclassb5_b6/adapters/PageAdapter.kt
@@ -0,0 +1,22 @@
+package com.xiangze.mobileclassb5_b6.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class PageAdapter(
+    val fragments: List<Fragment>,
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+): FragmentStateAdapter(fragmentManager, lifecycle){
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
+
+
