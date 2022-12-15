de85d9bd3833e5d9c02ab06fdadf028722344780
Caaron <arn_0000@hotmail.com>
2022-12-15 11:16:38 +0800

    testing

--- app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
+++ app/src/main/java/com/xiangze/mobileclassb5_b6/MainActivity.kt
@@ -2,7 +2,7 @@ package com.xiangze.mobileclassb5_b6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Caaron
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
