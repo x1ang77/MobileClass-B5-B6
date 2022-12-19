2a10b19245667c497fcb06e5196255cd86b173fd
khayrultw <khayrultw@gmail.com>
2022-12-19 11:58:28 +0600

    testing git reset

--- app/src/main/java/com/xiangze/mobileclassb5_b6/fragments/FirstFragment.kt
+++ app/src/main/java/com/xiangze/mobileclassb5_b6/fragments/FirstFragment.kt
@@ -1,18 +1,28 @@
 package com.xiangze.mobileclassb5_b6.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.xiangze.mobileclassb5_b6.R
+import com.xiangze.mobileclassb5_b6.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvTest.setBackgroundColor(Color.parseColor("#FF0000"))
     }
 }
